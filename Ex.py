01f3ec34d5cdcb8e71dfa3078685f2d2f4e7a560
naveen25895 <naveen25895@gmail.com>
2020-07-06 17:39:37 +0530

    Added python file

---
+++ Ex.py
@@ -0,0 +1,33 @@
+#  .   ,
+# 'Varibles is a containers for a value'
+'''
+1)Arthmetic operator
+        --
+        + _ * / //(floor division)  %(modulus) Exponential(**)
+2)Relatiional operator
+        -- True or False
+        > < <= >= == !=
+3)Assignment operator
+        --
+        = += -= /= *= %= **= //=
+4)Logical operator
+        -- res True or false
+        and or not
+5)Membership Operator
+    --res True or False
+        in  notin
+6)Identity Operator
+    -- res True or False
+        is isnot
+7)Bitwise Operator
+    --
+    AND(&) Or(|) Xor(^) complement(~) leftshift(<<) rightshift(>>)
+'''
+'''
+2 - 10
+3 - 11
+10 - 1010
+8 - 1000
+'''
+print(ord('$'))
+
